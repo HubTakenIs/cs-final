1d047145033d3b7784ace9490ec281411b2373b6
HubTakenIs <hubertaslol@gmail.com>
2025-07-07 17:04:18 +0100

    added list view.

--- note_app/napp/note.py
+++ note_app/napp/note.py
@@ -1,25 +1,51 @@
-from flask import Blueprint
-from . import db
+from flask import Blueprint, render_template, flash, request, redirect, url_for, g
+from napp.db import get_db
+
 
 bp = Blueprint('note', __name__ ,url_prefix='/note')
 
 
-@bp.route('/create')
-def create_note():
-    return 'create note'
+@bp.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO note (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('note.list'))
+    return render_template('note/create.html')
 
 @bp.route('/<int:id>/view')
 def single_note(id):
     return f'single {id} note view'
 
 @bp.route('/list')
-def list_view():
-    return 'note list'
+def list():
+    db = get_db()
+    notes = db.execute(
+        'SELECT n.id, title, body, created, author_id, username'
+        ' FROM note n JOIN user u ON n.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('note/list.html',notes=notes)
 
 @bp.route('/<int:id>/update')
-def update_note(id):
+def update(id):
     return f'update {id} note'
 
 @bp.route('/<int:id>/delete')
-def delete_note(id):
+def delete(id):
     return f'delete {id} note'
